a3e3a58c2f5f6806e217248351962f626a123d18
Monica Momiy <mmomiy@gmail.com>
2012-03-15 21:43:21 -0500

    correccion solucion pregunta 4

--- db/migrate/20120316020037_create_users.rb
+++ db/migrate/20120316020037_create_users.rb
@@ -5,7 +5,6 @@ class CreateUsers < ActiveRecord::Migration
       t.string :lastname
       t.string :email
       t.string :birthdate
-      t.references :role
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120316020037) do
+ActiveRecord::Schema.define(:version => 20120316023945) do
 
   create_table "roles", :force => true do |t|
     t.string   "description"
@@ -19,12 +19,16 @@ ActiveRecord::Schema.define(:version => 20120316020037) do
     t.datetime "updated_at",  :null => false
   end
 
+  create_table "roles_users", :id => false, :force => true do |t|
+    t.integer "role_id"
+    t.integer "user_id"
+  end
+
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "lastname"
     t.string   "email"
     t.string   "birthdate"
-    t.integer  "role_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
