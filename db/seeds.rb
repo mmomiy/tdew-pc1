5ab1317b415133ea52f6e6605be031f0b505a417
Monica Momiy <mmomiy@gmail.com>
2012-03-15 22:01:45 -0500

    solucion pregunta 5

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+users = User.create([{ name:"Monica",lastname:"Momiy",email:"mmomiy@gmail.com",birthdate:"11041985" }, 
+	{ name:"Arturo",lastname:"Momiy",email:"rmomiy@gmail.com",birthdate:"02021987" },
+	{ name:"Ricardo",lastname:"Momiy",email:"imomiy@gmail.com",birthdate:"29031986" }])
+
+admins = User.create([{ name:"Lucia",lastname:"Momiy",email:"lmomiy@gmail.com",birthdate:"11081985" },
+	{ name:"Raul",lastname:"Momiy",email:"rmomiy@gmail.com",birthdate:"02021987" }])
+
+Role.create(description: "Usuarios", users: users)
+Role.create(description: "Administrador", users: admins)
